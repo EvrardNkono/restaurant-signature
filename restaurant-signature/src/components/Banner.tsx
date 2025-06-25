c0c203fd3eed638abcff0d1387a96e1900191e1c
Evrard Nkono <evrardnkono870@gmail.com>
2025-06-25 16:59:37 +0100

    Ton message de commit global

--- restaurant-signature/src/components/Banner.tsx
+++ restaurant-signature/src/components/Banner.tsx
@@ -1,16 +1,30 @@
-// Banner.tsx
 import { useNavigate } from 'react-router-dom';
-import bannerBg from '../assets/images/banner-bg.jpg'; 
+import { useState, useEffect } from 'react';
 import './Banner.css';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUtensils, faStar, faAward } from '@fortawesome/free-solid-svg-icons';
+import { faUtensils, faStar, faAward, faEnvelope, faPhone, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+
+import banner1 from '../assets/images/banner-bg.jpg';
+import banner2 from '../assets/images/banner2.jpeg';
+import banner3 from '../assets/images/banner3.jpg';
+
+const images = [banner1, banner2, banner3];
 
 const Banner = () => {
   const navigate = useNavigate();
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 5000);
+
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
-    <div className="banner" style={{ backgroundImage: `url(${bannerBg})` }}>
+    <div className="banner" style={{ backgroundImage: `url(${images[currentImageIndex]})` }}>
       <div className="overlay" />
 
       <div className="banner-content">
@@ -24,16 +38,23 @@ const Banner = () => {
 
         <div className="btn-group">
           <button className="banner-button" onClick={() => navigate('/special-weekend')}>
-  <FontAwesomeIcon icon={faStar} className="icon green-icon" />
-  
-  Special<br /> Weekend
-</button>
+            <FontAwesomeIcon icon={faStar} className="icon green-icon" />
+            Special<br /> Weekend
+          </button>
 
           <button className="banner-button" onClick={() => navigate('/concept-chef')}>
             <FontAwesomeIcon icon={faAward} className="icon gold-icon" />
             Le concept<br />du Chef
           </button>
         </div>
+      </div>
+
+      {/* Carte Contact Flottante */}
+      <div className="contact-floating-card">
+        <h4>Contact</h4>
+        <p><FontAwesomeIcon icon={faPhone} className="contact-icon" /> <a href="tel:+33644951184">+33 6 44 95 11 84</a></p>
+        <p><FontAwesomeIcon icon={faEnvelope} className="contact-icon" /> <a href="mailto:restaurantsignature@outlook.fr">Mail</a></p>
+        <p><FontAwesomeIcon icon={faMapMarkerAlt} className="contact-icon" /> 13 rue St-Barthélemy, 77000 Melun </p>
       </div>
     </div>
   );
