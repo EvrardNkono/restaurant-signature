ecde023bb0d47cf3bd75cc39223fd5268eaa1855
Evrard Nkono <evrardnkono870@gmail.com>
2026-05-18 18:35:24 +0100

    all

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Instagram, Facebook, MessageCircle, MapPin, Clock, Phone } from "lucide-react";
-
+import "./footer.css";
 
 export default function Footer() {
   
@@ -10,11 +10,11 @@ export default function Footer() {
     window.dispatchEvent(new CustomEvent('openReservation'));
   };
 
-  // Horaires d'ouverture (MIS À JOUR)
+  // Horaires d'ouverture
   const getOpeningHours = () => {
     const days = [
       { name: "Lundi", open: false, lunch: null, dinner: null },
-      { name: "Mardi", open: true, lunch: { start: "12h00", end: "14h00" }, dinner: { start: "18h00", end: "23h00" } },
+      { name: "Mardi", open: true, lunch: { start: "12h00", end: " 14h00" }, dinner: { start: "18h00", end: "23h00" } },
       { name: "Mercredi", open: true, lunch: { start: "12h00", end: "14h00" }, dinner: { start: "18h00", end: "23h00" } },
       { name: "Jeudi", open: true, lunch: { start: "12h00", end: "14h00" }, dinner: { start: "18h00", end: "23h00" } },
       { name: "Vendredi", open: true, lunch: { start: "12h00", end: "14h00" }, dinner: { start: "18h00", end: "23h00" } },
@@ -116,8 +116,8 @@ export default function Footer() {
                 {getTodayHoursText()}
               </p>
               <div className="hours-list">
-                <p><span className="gold-text">Mardi - Vendredi</span> : 12h00 - 14h00 / 18h00 - 23h00</p>
-                <p><span className="gold-text">Samedi - Dimanche</span> : 12h00 - 14h00 / 18h00 - 00h00</p>
+                <p><span className="gold-text">Mardi - Vendredi</span> : 11h30 - 14h30 / 18h30 - 23h30</p>
+                <p><span className="gold-text">Samedi - Dimanche</span> : 11h30 - 14h30 / 18h30 - 00h30</p>
                 <p><span className="gold-text">Lundi</span> : Fermé</p>
               </div>
             </div>
